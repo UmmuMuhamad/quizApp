84223adc7731cd88d13e20398c5194688f45fe5c
IsaacWong <kidosol3839@gmail.com>
2020-03-12 21:51:07 +0800

    :

---
+++ quiz-script.js
@@ -0,0 +1,26 @@
+var questons = [{
+    "question": "1+2",
+    "option1": "1",
+    "option2": "4",
+    "option3": "5",
+    "option4": "3",
+    "answer": "3"
+
+}, {
+    "question": "1+4",
+    "option1": "5",
+    "option2": "2",
+    "option3": "1",
+    "option4": "3",
+    "answer": "5"
+
+}, {
+    "question": "4+6",
+    "option1": "5",
+    "option2": "10",
+    "option3": "1",
+    "option4": "3",
+    "answer": "10"
+
+}
+]
